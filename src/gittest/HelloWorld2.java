aa3348e8e82e5eefd5fcf4381a9c9bb0937c3aaa
AlvaShui <1241443212@qq.com>
2017-06-07 11:22:06 +0800

    add HelloWorld2.java

---
+++ src/gittest/HelloWorld2.java
@@ -0,0 +1,14 @@
+package gittest;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		new HelloWorld().test();
+	}
+
+	public void test() {
+		System.out.println("这是新增的test方法");
+	}
+}
